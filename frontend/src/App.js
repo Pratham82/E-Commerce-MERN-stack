73f33e1af1455950a675ff9cf9e9ffcd52853e6b
pratham82 <mali.prathamesh82@gmail.com>
2021-01-21 20:28:25 +0530

    Added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './components/screens/HomeScreen'
+import ProductScreen from './components/screens/ProductScreen'
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
           <h1 className="text-center"> Welcome to E-shop 🛒</h1>
-          <Home />
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
