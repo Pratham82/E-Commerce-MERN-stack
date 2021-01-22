80ace89c09680be189c6fb175d56509a5a9b1f16
pratham82 <mali.prathamesh82@gmail.com>
2021-01-22 18:49:59 +0530

    Added environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+dotenv.config()
+
 const app = express()
 
 app.get('/', (_, res) => res.send('Hello from express'))
 
-app.get('/products', (_, res) => res.json(products))
+app.get('/api/products', (_, res) => res.json(products))
 
-app.get('/products/:id', (req, res) => {
+app.get('/api/products/:id', (req, res) => {
   const productId = req.params.id
   const product = products.find(p => p._id === productId)
-  console.log(product)
   res.send(product)
 })
 
-app.listen(5000, console.log(`App Running on PORT 5000`))
+app.listen(
+  process.env.PORT || 5000,
+  console.log(`App Running on PORT ${process.env.PORT}`)
+)
